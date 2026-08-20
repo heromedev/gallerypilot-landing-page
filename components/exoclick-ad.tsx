901c46cddeb7400b170b3482bb76a0f509c9a754
MathisZerbib <mathis.zerbib@gmail.com>
2026-08-20 23:16:14 +0200

    fix ExoClick ad - use useEffect for script injection

--- components/exoclick-ad.tsx
+++ components/exoclick-ad.tsx
@@ -1,22 +1,32 @@
 'use client'
 
-import Script from 'next/script'
+import { useEffect, useRef } from 'react'
 
 export function ExoClickAd() {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!containerRef.current) return
+
+    const script1 = document.createElement('script')
+    script1.async = true
+    script1.type = 'application/javascript'
+    script1.src = 'https://a.magsrv.com/ad-provider.js'
+    document.body.appendChild(script1)
+
+    const script2 = document.createElement('script')
+    script2.textContent = `(AdProvider = window.AdProvider || []).push({"serve": {}});`
+    document.body.appendChild(script2)
+
+    return () => {
+      script1.remove()
+      script2.remove()
+    }
+  }, [])
+
   return (
-    <>
-      <Script
-        async
-        type="application/javascript"
-        src="https://a.magsrv.com/ad-provider.js"
-        strategy="afterInteractive"
-      />
-      <div className="flex justify-center py-8">
-        <ins className="eas6a97888e2" data-zoneid="6007880" />
-      </div>
-      <Script id="exoclick-init" strategy="afterInteractive">
-        {`(AdProvider = window.AdProvider || []).push({"serve": {}});`}
-      </Script>
-    </>
+    <div ref={containerRef} className="flex justify-center py-8">
+      <ins className="eas6a97888e2" data-zoneid="6007880" />
+    </div>
   )
 }
